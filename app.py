c8f4dc15535690fbb2aef4ff5b3e2d386503d5ab
Carson <carson@mcmumf.dev>
2025-11-12 19:35:20 -0500

    Start of events feed

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     amc_response = get_events()
     return render_template('index.html', events=format_events(amc_response))
 
+@app.route('/events')
+def events():
+    amc_response = get_events()
+    return render_template('events.html', events=format_events(amc_response))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
